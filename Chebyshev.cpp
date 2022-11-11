332584bc42ee402ec896c53c68881dc51e8aefc1
dekel98s <dekel98@gmail.com>
2022-11-11 17:20:06 +0200

    Manhattan+Chebyshev

--- Chebyshev.cpp
+++ Chebyshev.cpp
@@ -5,14 +5,18 @@
 #include "iostream"
 #include "cmath"
 #include "Utils.cpp"
+
 using namespace std;
 
-void Chebyshev () {
-    int dim;
-    cout << "Please select how many dimensions" << endl;
-    cin >> dim;
-    int *p = new int[dim];
-    int *q = new int[dim];
+void Chebyshev(int *v1, int *v2, int size) {
+    int max = 0;
+    int tempSum;
+    for (int i = 0; i < size; i++) {
+        tempSum=fabs(v1[i] - v2[i]);
+        if(tempSum>max){
+            max=tempSum;
+        }
+    }
+    cout << max << endl;
 
-    return;
 }
--- Manhattan.cpp
+++ Manhattan.cpp
@@ -5,13 +5,13 @@
 #include "iostream"
 #include "cmath"
 #include "Utils.cpp"
+
 using namespace std;
 
-void Manhattan() {
-    int dim;
-    cout << "Please select how many dimensions" << endl;
-    cin >> dim;
-    int *p = new int[dim];
-    int *q = new int[dim];
-    return;
+void Manhattan(int *v1, int *v2, int size) {
+    int sum = 0;
+    for (int i = 0; i < size; i++) {
+        sum += fabs(v1[i] - v2[i]);
+    }
+    cout << sum << endl;
 }
