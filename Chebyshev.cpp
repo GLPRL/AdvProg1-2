2ca0864348720404ed8be95e3aef44392a5a4d9b
GLPRL <galpearl24@gmail.com>
2022-11-13 17:17:07 +0200

    Updated arguments, to be of type Vector, instead of simple arrays

--- Chebyshev.cpp
+++ Chebyshev.cpp
@@ -1,13 +1,16 @@
 #include "iostream"
 #include "cmath"
 #include <iomanip>
+#include <vector>
 using namespace std;
+
 /**
- receives 2 vectors represented by arrays and their size.
+ receives 2 vectors.
  calculates their Chebyshev distance, and prints it.
  */
-void chebyshev(int *v1, int *v2, int size) {
+void chebyshev(vector<int> v1, vector<int> v2) {
     double max = 0;
+    int size=v1.size();
     int tempSum;
     for (int i = 0; i < size; i++) {
         tempSum = fabs(v1[i] - v2[i]);
--- Manhattan.cpp
+++ Manhattan.cpp
@@ -1,14 +1,16 @@
 #include "iostream"
 #include "cmath"
 #include <iomanip>
-/**
- receives 2 vectors represented by arrays and their size.
- calculates their Manhattan distance and prints it.
- */
+#include <vector>
 using namespace std;
 
-void manhattan(int *v1, int *v2, int size) {
+/**
+ * receives 2 vectors.
+ * calculates their Manhattan distance and prints it.
+ */
+void manhattan(vector<int> v1, vector<int> v2) {
     double sum = 0;
+    int size = v1.size();
     for (int i = 0; i < size; i++) {
         sum += fabs(v1[i] - v2[i]);
     }
