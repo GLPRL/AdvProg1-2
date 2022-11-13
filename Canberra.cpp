30444290d55754c0e84674416fcdc3d2e4fb2c68
GLPRL <galpearl24@gmail.com>
2022-11-13 16:30:29 +0200

    Updated arguments, to be of type Vector, instead of custom

--- Canberra.cpp
+++ Canberra.cpp
@@ -1,26 +1,24 @@
-/*
- * Created by Gal Pearl on 11/11/2022.
- * The file will run a Canberra Distance calculation procedure.
-*/
+
 #include "iostream"
 #include "cmath"
 #include <iomanip>
+#include <vector>
 using namespace std;
+
 /**
  * Canberra Distance algorithm. Prints the distance between two vectors,
  * With precision of 1 digit to the right of the decimal point.
- * @param v1
- * @param v2
- * @param dim
+ * @param v1 First vector.
+ * @param v2 Second vector.
  */
-void canberra(int* v1, int* v2, int dim) {
-    double *dist = new double[dim];
-    for (int i = 0; i < dim; i++) {                             //Inner calculations for division.
-        dist[i] = fabs(v1[i] - v2[i]) / (abs(v1[i]) + abs(v2[i]));
+void canberra(vector<int> v1, vector<int> v2) {
+    vector<double> dist;
+    for (int i = 0; i < v1.size(); i++) {                               //Inner calculations for division.
+        dist.push_back(fabs(v1[i] - v2[i]) / (abs(v1[i]) + abs(v2[i])));
     }
     double sum = 0;
-    for (int i = 0; i < dim; i++) {                             //Summary of elements according to the formula,
-        sum += dist[i];                                         //Repr. the sum of divisions.
+    for (int i = 0; i < v1.size(); i++) {                               //Summary of elements according to the formula,
+        sum += dist[i];                                                 //Repr. the sum of divisions.
     }
     cout << fixed << setprecision(1) << sum << endl;
 }
