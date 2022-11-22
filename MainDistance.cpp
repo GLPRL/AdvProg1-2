d75b7366cd6bb64068f1669cad1a06c6e735659c
GLPRL <galpearl24@gmail.com>
2022-11-22 20:23:47 +0200

    Grammar changes, user prompt messages

--- MainDistance.cpp
+++ MainDistance.cpp
@@ -2,10 +2,8 @@
  Receives two vectors from the user and calculates various types of distances between them.
 */
 #include "Algorithms.h"
-#include <iostream>
 
 using namespace std;
-
 
 /**
  Reads a vector from the user in accordance to the ex1 instructions.
@@ -15,22 +13,22 @@ vector<double> readVector() {
     string lin;
     getline(cin, lin);
     lin = ' ' + lin + " ";
-    //if there is more than 1 space in a row exits the program.
+    // If there is more than one space in a row, then exit the program.
     if (lin.find("  ") != string::npos) {
-        printf("Error: too many blankspaces ");
+        cout << "Error: too many white spaces" << endl;
         exit(-3);
     }
     vector<double> v;
     int pos = 0;
     double x;
     char *e;
-    // loop untill the end of the string each time seprating the spaces.
+    // Loop until the end of the string each time separating the spaces.
     while ((pos = lin.find(" ")) != string::npos) {
         string sub = lin.substr(0, pos);
         x = std::strtod(sub.c_str(), &e);
         if (*e != '\0')
         {
-            printf("Error: %s is not a number\n", sub.c_str());
+            cout << "Error:" << sub.c_str() << " is not a number" << endl; ;
             exit(-1);
         }
         v.push_back(x);
@@ -38,9 +36,14 @@ vector<double> readVector() {
     }
     return v;
 }
-
+/**
+ * Main function. Summons the vector builders, checks if they are valid, and distance algorithms.
+ * @return code 0 if works as expected.
+ */
 int main() {
+    cout << "Please enter first vector" << endl;
     vector<double> v1 = readVector();
+    cout << "Please enter second vector" << endl;
     vector<double> v2 = readVector();
     if (v1.size() != v2.size()) {
         cout << "Error: Vectors are not the same size" << endl;
@@ -55,5 +58,5 @@ int main() {
     cout.precision(5);
     cout.unsetf(std::ios_base::floatfield);
     cout << minkowski(v1, v2) << endl;
-
+    return 0;
 }
